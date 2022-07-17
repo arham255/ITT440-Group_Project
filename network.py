32b325e9f98459f91a36d06988487bd157f86051
arham255 <2021497908@student.uitm.edu.my>
2022-07-17 16:49:21 +0800

    Connect client with server

---
+++ network.py
@@ -0,0 +1,28 @@
+import socket
+import pickle
+
+
+class Network:
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "192.168.210.191"
+        self.port = 5555
+        self.addr = (self.server, self.port)
+        self.conn = self.connect()
+
+    def getConn(self):
+        return self.conn
+
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return self.client.recv(2048).decode()
+        except:
+            pass
+
+    def send(self, data):
+        try:
+            self.client.send(str.encode(data))
+            return pickle.loads(self.client.recv(2048*2))
+        except socket.error as err:
+            print(err)
